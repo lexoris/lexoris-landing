c8867179c7e9cd03a505a69b5b6f4e61b4c920c1
lexoris <admin@lexoris.com.au>
2026-01-16 16:53:13 +1100

    Add files via upload

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+// Shared constants for the Lexoris website
+
+export const DEMO_BOOKING_URL = "https://calendly.com/admin-lexoris/10min";
+export const DEMO_BOOKING_LABEL = "Book 10-min demo";
+export const DEMO_BOOKING_ARIA_LABEL = "Book 10-minute Lexoris demo";
